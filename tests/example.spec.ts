dbc7c64471743b207c8236b2b4eb028256ceaf93
popadriangeo <adrian.pop.ext@vitagroup.com>
2022-10-27 12:58:06 +0300

    upd

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Quick Leaf privacy policy verification ', async ({ page }) => {
+  // reaches the tested page
   await page.goto('https://www.quickleaf.net/');
 
   // Expect a title "to contain" a substring.
@@ -12,11 +13,16 @@ test('Quick Leaf privacy policy verification ', async ({ page }) => {
   // Expect an attribute "to be strictly equal" to the value.
   await expect(privacyPolicy).toHaveAttribute('href', 'https://www.quickleaf.net/privacy-policy/');
 
+  //clicks on privacy policy button and verifies new popup page
   const [privacyPolicyPageHandle] = await Promise.all([
     page.waitForEvent('popup'),
     privacyPolicy.click()
   ]);
 
-  // Expects the URL to contain intro.
+  // Verifies page redirects to the appropriate Url
   await expect(privacyPolicyPageHandle).toHaveURL('https://www.quickleaf.net/privacy-policy/');
+
+  //Declare privacy policy locator and Accepts Privacy Policy
+  const acceptPrivacyPolicy = page.locator('text="Accept"');
+  await acceptPrivacyPolicy.click();
 });
